d3deb1bd9ca3ce9c53e75fa13f3d77d357651b6e
Kamil <kami.okowiak2001@gmail.com>
2026-05-29 09:49:20 +0200

    added problems section

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@ const hamburger = document.querySelector('.hamburger')
 const offcanvas = document.querySelector('.offcanvas')
 
 hamburger.addEventListener('click', () => {
-	hamburger.classList.toggle('is-active') // klasa biblioteki hamburgers — animuje krzyżyk
+	hamburger.classList.toggle('is-active')
 	offcanvas.classList.toggle('offcanvas--active')
+	document.body.classList.toggle('no-scroll')
 })
